5f56a54b5af6f4610e24c3685b6874771f556476
MykeStv <mykestv22@gmail.com>
2022-04-05 14:27:20 -0500

    book model

---
+++ src/main/java/com/myke/library/model/Book.java
@@ -0,0 +1,87 @@
+package com.myke.library.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "books")
+public class Book {
+
+    // ATTRIBUTES
+    @Id
+    private String id;
+    private String isbn;
+    private String name;
+    private String author;
+    private String category;
+    private String type;
+    private Boolean borrowed;
+    private LocalDate dateBorrewed;
+
+    
+    // GETTER && SETTER
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(Boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public LocalDate getDateBorrewed() {
+        return dateBorrewed;
+    }
+
+    public void setDateBorrewed(LocalDate dateBorrewed) {
+        this.dateBorrewed = dateBorrewed;
+    }
+}
